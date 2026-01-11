5abd01b6ed23193d4b290e8a2230cfb0055bf369
Joshua Pozzolungo <z5599337@ad.unsw.edu.au>
2026-01-11 18:54:18 +1100

    made some additions to drop down

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,17 +5,19 @@ export default function NavBar({ toggleMobileMenu, isMobileMenuOpen }) {
   
   const [isClicked, setIsClicked] = useState(false);
   
-  const dropDownMenuItem = () => {
-    <div className="flex justify-center items-center space-x-3 m-4 hover:bg-secondary-box hover:border-1 hover:border-white/20 hover:rounded-lg hover:border-dashed cursor-pointer">
-      <div className="flex justify-center items-center h-[45px] w-[45px] border-1 border-white/20 rounded-lg ">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-files-icon lucide-files"><path d="M15 2h-4a2 2 0 0 0-2 2v11a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V8"/><path d="M16.706 2.706A2.4 2.4 0 0 0 15 2v5a1 1 0 0 0 1 1h5a2.4 2.4 0 0 0-.706-1.706z"/><path d="M5 7a2 2 0 0 0-2 2v11a2 2 0 0 0 2 2h8a2 2 0 0 0 1.732-1"/></svg>
-      </div>
-        <div>
-          <h1>Questions</h1>
-          <p className="text-primary-text opacity-60">Quality Exam Questions</p>
+  const DropDownMenuItem = ({heading, description}) => {
+    return (
+      <div className="flex justify-center items-center space-x-3 p-3 m-4 hover:bg-secondary-box hover:border-1 hover:border-white/20 hover:rounded-lg hover:border-dashed cursor-pointer">
+        <div className="flex justify-center items-center h-[45px] w-[45px] border-1 border-white/20 rounded-lg ">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-files-icon lucide-files"><path d="M15 2h-4a2 2 0 0 0-2 2v11a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V8"/><path d="M16.706 2.706A2.4 2.4 0 0 0 15 2v5a1 1 0 0 0 1 1h5a2.4 2.4 0 0 0-.706-1.706z"/><path d="M5 7a2 2 0 0 0-2 2v11a2 2 0 0 0 2 2h8a2 2 0 0 0 1.732-1"/></svg>
         </div>
-    </div>
-  }
+          <div>
+            <h1>{heading}</h1>
+            <p className="text-primary-text opacity-60">{description}</p>
+          </div>
+      </div>
+    );
+  };
 
   return (
     <header className="sticky top-0 z-50 mx-auto min-h-16 w-full backdrop-blur-2xl pt-3 pb-3 ">
@@ -32,21 +34,12 @@ export default function NavBar({ toggleMobileMenu, isMobileMenuOpen }) {
               </button>
               <ChevronDown className={`transition-transform duration-300 ${isClicked ? 'rotate-180' : 'rotate-0'}`}/>
             </div>
-            <div className={`${isClicked ? '' : 'hidden'} absolute mt-3 grid grid-cols-2 h-50 w-150 border-1 border-white/20 rounded-lg`}>
-              <div className="flex justify-center items-center space-x-3 m-4 hover:bg-secondary-box hover:border-1 hover:border-white/20 hover:rounded-lg hover:border-dashed cursor-pointer">
-                <div className="flex justify-center items-center h-[45px] w-[45px] border-1 border-white/20 rounded-lg ">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-files-icon lucide-files"><path d="M15 2h-4a2 2 0 0 0-2 2v11a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V8"/><path d="M16.706 2.706A2.4 2.4 0 0 0 15 2v5a1 1 0 0 0 1 1h5a2.4 2.4 0 0 0-.706-1.706z"/><path d="M5 7a2 2 0 0 0-2 2v11a2 2 0 0 0 2 2h8a2 2 0 0 0 1.732-1"/></svg>
-                </div>
-                <div>
-                  <h1>Questions</h1>
-                  <p className="text-primary-text opacity-60">Quality Exam Questions</p>
-                </div>
-              </div>
-
-              <a>Exercises</a>
-              <a>Videos</a>
-              <a>Playlists</a>
-              <a>Tutoring</a>
+            <div className={`${isClicked ? '' : 'hidden'} absolute mt-3 grid grid-cols-2 h-75 w-150 border-1 border-white/20 rounded-lg`}>
+              <DropDownMenuItem heading="Questions" description="Quality Exam Questions"/>
+              <DropDownMenuItem heading="Questions" description="Quality Exam Questions"/>
+              <DropDownMenuItem heading="Questions" description="Quality Exam Questions"/>
+              <DropDownMenuItem heading="Questions" description="Quality Exam Questions"/>
+              <DropDownMenuItem heading="Questions" description="Quality Exam Questions"/>
             </div>
           </div>
           <a className="p-2 cursor-pointer hover:underline">Leaderboard</a>
